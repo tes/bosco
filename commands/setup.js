34bfdb8404d7ccab2cd88e96ee5b814e8a380220
cliftonc <clifton.cunningham@gmail.com>
2015-10-05 08:55:18 +0100

    Update setup to remove link

--- commands/setup.js
+++ commands/setup.js
@@ -10,14 +10,11 @@ function cmd(bosco, args) {
     var clone = require('./clone');
     var install = require('./install');
     var team = require('./team');
-    var link = require('./link');
 
     team.cmd(bosco, ['sync'], function() {
       team.cmd(bosco, ['setup'], function() {
-        clone.cmd(bosco, [], function() {
-            link.cmd(bosco, [], function() {
-                install.cmd(bosco, args);
-            });
+        clone.cmd(bosco, [], function() {            
+            install.cmd(bosco, args);            
         });
       });
     });
