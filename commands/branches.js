4835df176fbd6d85acc8ba827c39d43496d273a7
andy-duncan <andy.duncan@tesglobal.com>
2016-10-06 12:58:27 +0200

    change to use 'git rev-parse'

--- commands/branches.js
+++ commands/branches.js
@@ -3,16 +3,16 @@ var _ = require('lodash');
 
 module.exports = {
   name: 'branches',
-  description: 'Checks git branch list across all services',
+  description: 'Checks git local branch name across all services',
   usage: '[-r <repoPattern>]',
 };
 
 function cmd(bosco) {
-  bosco.log('Running \'git branch --list\' across all matching repos ...');
+  bosco.log('Running \'git rev-parse --abbrev-ref HEAD\' across all matching repos ...');
 
   var options = ch.createOptions(bosco, {
     cmd: 'git',
-    args: ['branch', '--list'],
+    args: ['rev-parse', '--abbrev-ref', 'HEAD'],
     guardFn: function(innerBosco, repoPath, opts, next) {
       if (innerBosco.exists([repoPath, '.git'].join('/'))) return next();
       next(new Error('Doesn\'t seem to be a git repo: ' + repoPath.blue));
@@ -20,11 +20,10 @@ function cmd(bosco) {
     stdoutFn: function(stdout, path) {
       if (!stdout) return;
 
-      // find the line in the stdout with the * in front, and grab the branch name
-      var activeBranch = stdout.match(/\* (.*)/)[1];
+      var branchName = stdout.trim();
 
-      if (activeBranch !== 'master') {
-        bosco.log(path.blue + ' is on branch \'' + activeBranch.cyan + '\'');
+      if (branchName !== 'master') {
+        bosco.log(path.blue + ' is on branch \'' + branchName.cyan + '\'');
       }
     },
   });
