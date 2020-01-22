265e4cc11a85d6c84d9ffcb3a060fc175ce051db
Chris Flook <chris.flook@tesglobal.com>
2020-01-22 09:48:16 +0000

    Update/change linting... commit so I've somewhere to go back to

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: false,
+    commonjs: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+  },
+};
